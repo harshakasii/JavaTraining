89bd954fa8011acd05af541841fb14c34edad399
harsha kasi <harshakasi2003@gmail.com>
2024-10-15 21:56:50 +0530

    Q5 is added

--- src/Oops/Q5CallingConstructorFromConstructor.java
+++ src/Oops/Q5CallingConstructorFromConstructor.java
@@ -1,4 +1,30 @@
 package Oops;
 
 public class Q5CallingConstructorFromConstructor {
+    public static void main(String[] args) {
+
+        Test ss = new Test();
+        System.out.println(ss.age);
+        System.out.println(ss.name);
+
+    }
+}
+
+
+
+class Test {
+
+    int age;
+    String name;
+
+
+    Test() {
+        this(23, "Harsha");
+    }
+
+
+    Test(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
 }
