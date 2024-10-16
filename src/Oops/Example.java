f54fc3822cbecff5b35a403a4973512e2cc67317
harshakasii1 <harsha.kasi@capgemini.com>
2024-10-16 10:01:25 +0530

    Q6 added

--- src/Oops/Example.java
+++ src/Oops/Example.java
@@ -1,4 +1,17 @@
 package Oops;
 
 public class Example {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        swap(a,b);
+        System.out.println(a);
+        System.out.println(b);
+    }
+    static void swap(int a, int b) {
+        int temp = a;
+        a=b;
+        b = temp;
+    }
 }
---
+++ src/Oops/Q6FinalKeyword.java
@@ -0,0 +1,12 @@
+package Oops;
+
+public class Q6FinalKeyword {
+    public static void main(String[] args) {
+        final int A = 10;
+        //A = 3;
+        /*
+        when we try to change the a value it won't be changed as it is final.
+         */
+        System.out.println(A);
+    }
+}
