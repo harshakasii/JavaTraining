5ede2b6e0019745c36ce0d5de0479eeada2b3112
harshakasii1 <harsha.kasi@capgemini.com>
2024-10-16 19:08:08 +0530

    Strings added

---
+++ src/Strings/Q3StringContentComparison.java
@@ -0,0 +1,12 @@
+package Strings;
+
+public class Q3StringContentComparison {
+    public static void main(String[] args) {
+        String a = new String("Harsha");
+        String b = new String("Harsha");
+        System.out.println(a.equals(b));
+//        /*
+//        .equals() method check whether the content is same or not.
+//         */
+    }
+}
---
+++ src/Strings/Q4CharAtFunction.java
@@ -0,0 +1,10 @@
+package Strings;
+
+public class Q4CharAtFunction {
+    public static void main(String[] args) {
+        String name = "Harsha";
+        String ss = new String("Sam");
+        System.out.println(name.charAt(1));
+        System.out.println(ss.charAt(0));
+    }
+}
